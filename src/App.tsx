2d8ba11ea83dcb81e1b651e6831632f050221ca8
Diego <diegolima.almeida93@gmail.com>
2023-07-19 13:49:14 -0300

    feat: add dark mode theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Header } from "./components";
 
 export const App = () => {
   return (
-    <div className="bg-light-blue2 text-light-blue12">
+    <div className="bg-light-blue2 text-light-blue12 dark:bg-dark-blue2 dark:text-dark-blue12">
       <Header />
     </div>
   );
