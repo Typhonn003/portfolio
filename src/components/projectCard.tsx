5cb7e9ba3f0c0869a721b707b28bb4e3f0e4ec63
Diego <diegolima.almeida93@gmail.com>
2023-08-04 16:29:53 -0300

    feat: improved visual appearance of project card

--- src/components/projectCard.tsx
+++ src/components/projectCard.tsx
@@ -1,22 +1,49 @@
+import { BsGithub } from "react-icons/bs";
+
 export const ProjectCard = () => {
   return (
-    <ul className="group w-full max-w-[340px] h-[190px] overflow-hidden rounded-md border-2 border-light-blue7 relative dark:border-dark-blue7">
-      <div className="h-full transition-all duration-500 group-hover:h-0">
+    <li className="list-none w-full flex flex-col border border-light-blue7 rounded-md overflow-hidden">
+      <div className="h-40">
         <img
           src="src/img/placeholder.jpg"
-          alt=""
-          className="w-full h-full object-cover"
+          alt="Descrição da imagem"
+          className="h-full w-full object-cover object-left-top"
         />
-        <h2 className="absolute top-0 right-0 text-xl font-bold bg-light-blue9 px-4 py-2 rounded-bl-lg uppercase dark:bg-dark-blue9">
-          Hub LinkUp
-        </h2>
       </div>
-      <div className="h-full bg-light-blue9 dark:bg-dark-blue9">
-        <p className="px-4 pb-4 pt-14 text-light-blue1">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-          provident minus at nihil. Ea deserunt nesciunt aperiam.
+      <div className="flex flex-col gap-3 justify-between p-3">
+        <h2 className="font-semibold text-[20px]">Nome do projeto</h2>
+        <p className="text-sm leading-normal self-start">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum hic
+          delectus nam corporis consectetur, illum fugiat maxime incidunt harum
+          ducimus nostrum officia consequuntur esse vel voluptatem magnam sed
+          iste. Molestias!
         </p>
+        <ul className="text-light-blue12 flex flex-wrap gap-1 cursor-default">
+          <li className="text-xs rounded-full px-3 py-2 bg-light-blue11/30 font-medium">
+            React
+          </li>
+          <li className="text-xs rounded-full px-3 py-2 bg-light-blue11/30 font-medium">
+            Typescript
+          </li>
+          <li className="text-xs rounded-full px-3 py-2 bg-light-blue11/30 font-medium">
+            Styled Components
+          </li>
+        </ul>
       </div>
-    </ul>
+      <div>
+        <ul className="flex h-10 bg-light-blue3 border-t border-light-blue8">
+          <li className="flex-grow flex justify-center items-center border-r border-light-blue6">
+            <a href="#" target="_blank">
+              Demonstração
+            </a>
+          </li>
+          <li className="flex-grow flex justify-center items-center">
+            <a href="#" target="_blank" className="flex items-center gap-2">
+              Repositório <BsGithub />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </li>
   );
 };
