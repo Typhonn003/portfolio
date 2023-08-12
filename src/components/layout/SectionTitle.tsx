ffedb24865b59b38f9e150ac896d66855c729720
Diego <diegolima.almeida93@gmail.com>
2023-08-12 20:41:47 -0300

    feat: section title desktop style

--- src/components/layout/SectionTitle.tsx
+++ src/components/layout/SectionTitle.tsx
@@ -4,7 +4,7 @@ interface SectionTitleProps {
 
 export const SectionTitle = ({ title }: SectionTitleProps) => {
   return (
-    <div className="backdrop-blur-lg bg-light-blue2/40 sticky top-0 z-20 py-5 dark:bg-dark-blue2/40 lg:static lg:py-0 lg:pb-5">
+    <div className="backdrop-blur-lg bg-light-blue2/40 sticky top-0 z-20 py-5 dark:bg-dark-blue2/40 lg:static lg:py-0 lg:pb-5 lg:backdrop-blur-none lg:bg-transparent lg:dark:bg-transparent">
       <h2 className="mobile-width m-auto text-2xl font-bold">{title}</h2>
     </div>
   );
