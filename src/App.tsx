70acd4f49bc87d9664d5e4fa5a4400a02df50980
Diego <diegolima.almeida93@gmail.com>
2023-08-14 15:01:11 -0300

    feat: wallpaper in all modes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import ScrollSpy from "react-ui-scrollspy";
 
 export const App = () => {
   return (
-    <div className="bg-light-blue2 text-light-blue12 dark:bg-dark-blue2 dark:text-dark-blue12">
+    <div className="bg-light-blue2 text-light-blue12 dark:bg-dark-blue2 dark:text-dark-blue12 bg-[url('src/img/wallpaper-mobile.png')] bg-no-repeat bg-contain md:bg-[url('src/img/wallpaper-tablet.png')] lg:bg-[url('src/img/wallpaper-desktop.png')] lg:bg-fixed">
       <div className="lg:min-h-screen lg:max-w-7xl lg:m-auto lg:px-16">
         <div className="lg:flex lg:justify-between lg:gap-4">
           <div className="lg:max-h-screen lg:w-2/5 lg:sticky lg:top-0 lg:py-24">
