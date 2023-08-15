4ff4e95d2bc6e3e5257025f49c42da9a407b6b5b
Diego <diegolima.almeida93@gmail.com>
2023-08-15 00:25:54 -0300

    update: about section text

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -6,15 +6,16 @@ export const About = () => {
       <SectionTitle title="Sobre mim" />
       <article className="container-width m-auto flex flex-col gap-6 text-base leading-normal text-light-blue12/70 dark:text-dark-blue12/70 lg:w-full">
         <p>
-          O meu primeiro contato com a tecnologia ocorreu durante a minha
-          infância, quando meu pai me levou a uma lanhouse pela primeira vez. Eu
-          fiquei fascinado com aquele mundo e desde então, minha paixão por
-          tecnologia cresceu ano após ano.
+          Meu primeiro contato com a tecnologia ocorreu durante a minha
+          infância, quando meu pai me levou a uma LAN house pela primeira vez.
+          Ver todos aqueles computadores, repletos de jogos e possibilidades
+          quase infinitas, me deixou completamente hipnotizado. Daquele dia em
+          diante, não os deixei de lado nem por um único dia sequer.
         </p>
         <p>
           Aos 26 anos, meu irmão me incentivou a estudar o básico de
           desenvolvimento web. Alguns meses depois, um amigo que já atuava na
-          área sugeriu que eu expandisse meus conhecimentos na renomada{" "}
+          área sugeriu que eu estudasse na incrível{" "}
           <a
             href="https://kenzie.com.br/"
             target="_blank"
@@ -22,22 +23,19 @@ export const About = () => {
           >
             Kenzie Academy Brasil
           </a>
-          , um curso abrangente que engloba desde os fundamentos até tecnologias
-          avançadas de front-end e back-end.
+          . Eles oferecem um curso fantástico que abrange desde os fundamentos
+          até tecnologias avançadas de front end e back end, além de
+          proporcionar treinamento em soft skills e hard skills.
         </p>
         <p>
-          Hoje,{" "}
-          <strong className="text-light-blue12 dark:text-dark-blue12">
-            sou um desenvolvedor full stack formado
-          </strong>{" "}
-          pela Kenzie Academy Brasil. Durante um ano de estudos intensos, criei{" "}
-          <strong className="text-light-blue12 dark:text-dark-blue12">
-            mais de 40 projetos individuais e em grupo
-          </strong>
-          , adquirindo proficiência em diversas tecnologias. Agora, estou em
-          busca da minha primeira oportunidade para colocar meu talento à prova
-          e contribuir significativamente com o conhecimento que adquiri ao
-          longo dessa jornada
+          Hoje, sou um desenvolvedor full stack formado pela Kenzie Academy
+          Brasil. Durante mais de 2 mil horas de estudos intensos, tive a
+          oportunidade de conhecer pessoas, criar mais de 40 projetos, tanto
+          individuais quanto em grupo, o que me permitiu adquirir experiência em
+          diversas tecnologias. Agora, estou em busca da minha primeira
+          oportunidade para colocar meu talento e conhecimento à prova,
+          transformando desafios em soluções inovadoras e contribuindo de
+          maneira significativa no mundo do desenvolvimento.
         </p>
       </article>
     </section>
